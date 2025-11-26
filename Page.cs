c77fdef3df3eb6fe062548ef87c355faf1e7c73f
Space Banana <tomasbessa@protonmail.com>
2025-11-26 21:54:14 +0000

    Page source file

---
+++ Page.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+  
+//Simple page collection code
+public class Page : MonoBehaviour
+{
+  public PagesControl pages_control;
+  public Transform player;
+  public MeshRenderer page_mesh;
+  public AudioSource sound;
+
+  void Update(){ //Rip OnMouseDown() you will be missed
+    if (!page_mesh.isVisible) {return;}
+    if (!Mouse.current.leftButton.wasPressedThisFrame) {return;}
+    if (Vector3.Distance(this.transform.position, player.position) > 2) {return;}
+    sound.Play();
+    pages_control.collectPage();
+    this.gameObject.active = false;
+  }
+}
