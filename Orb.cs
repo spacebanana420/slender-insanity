e3cf2514dba0cf45254e04b34fca9fa62aa452ab
Space Banana <tomasbessa@protonmail.com>
2025-11-27 16:47:31 +0000

    Add orb code

---
+++ Orb.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+//Floating orb
+public class Orb : MonoBehaviour
+{
+  private float base_height;
+  private float max_height;
+  private bool levitate = false;
+  private float levitate_speed = 0.1f;
+  
+  void Start() {
+    this.base_height = this.transform.position.y;
+    this.max_height = this.base_height+30;
+  }
+
+  void Update() {
+    Vector3 pos = this.transform.position;
+    if (!this.levitate) {
+      pos.y = this.base_height + Mathf.Sin(Time.time * 0.5f) * 0.1f;
+      this.transform.position = pos;
+      return;
+    }
+    if (pos.y >= this.max_height) {
+      this.gameObject.active = false;
+      return;
+    }
+    pos.y += this.levitate_speed * Time.deltaTime;
+    this.transform.position = pos;
+    this.levitate_speed += 0.05f * Time.deltaTime;
+    return;
+
+  }
+  
+  void levitateOrb() {
+    this.levitate = true;
+    //todo: play a sound
+  }
+}
