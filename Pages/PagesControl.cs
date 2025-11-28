65fcafa08065f6234644c6d509bec8b6ac898e36
Space Banana <tomasbessa@protonmail.com>
2025-11-28 21:47:55 +0000

    Adjust teleport stats

--- Pages/PagesControl.cs
+++ Pages/PagesControl.cs
@@ -23,8 +23,8 @@ public class PagesControl : MonoBehaviour
   float[] look_limits = {8, 7, 6, 5, 4, 3, 2};
   
   float[] teleport_limits = {30, 25, 20, 15, 10, 5, 5};
-  float[] forward_tp_limits = {120, 120, 120, 120, 60, 30, 15};
-  bool[] can_tp_forward = {false, false, false, true, true, true, true};
+  float[] forward_tp_limits = {120, 120, 120, 80, 60, 30, 15};
+  bool[] can_tp_forward = {false, false, true, true, true, true, true};
 
   float[] invisible_limits = {80, 90, 100, 110, 120, 120, 120};
   bool[] can_be_invisible = {true, true, true, true, true, false, false};
