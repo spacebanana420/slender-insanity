f553843646ef43f62ae0c961e6fb9f6c6414a2e2
Space Banana <tomasbessa@protonmail.com>
2025-12-01 21:57:21 +0000

    Increase orb levitation speed

--- Orb.cs
+++ Orb.cs
@@ -15,18 +15,19 @@ public class Orb : MonoBehaviour
 
   void Update() {
     Vector3 pos = this.transform.position;
-    if (!this.levitate) {
+    if (!this.levitate) { //Idle
       pos.y = this.base_height + Mathf.Sin(Time.time * 0.5f) * 0.1f;
       this.transform.position = pos;
       return;
     }
-    if (pos.y >= this.max_height) {
+    if (pos.y >= this.max_height) { //Disappear into the sky
       this.gameObject.active = false;
       return;
     }
+    //Levitate
     pos.y += this.levitate_speed * Time.deltaTime;
     this.transform.position = pos;
-    this.levitate_speed += 1.5f * Time.deltaTime;
+    this.levitate_speed += 3f * Time.deltaTime;
     return;
 
   }
