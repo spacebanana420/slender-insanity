2ddc9e93c0c69adb81b4b73cfd6f5f0ba9486b37
Space Banana <tomasbessa@protonmail.com>
2025-12-08 19:01:15 +0000

    Move and rename Init code

---
+++ ObjectInit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+//Sets which objects are supposed to be active on scene start and which are not
+//Makes sure objects are correctly set up regardless of what is done in the editor
+public class ObjectInit : MonoBehaviour
+{
+  
+  public GameObject[] o_enable;
+  public GameObject[] o_disable;
+  
+  void Start() {
+    foreach (GameObject o in o_enable) {o.active = true;}
+    foreach (GameObject o in o_disable) {o.active = false;}
+  }
+}
