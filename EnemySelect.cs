31eab7129ed147d441e0c2a54428a2f395ecaa22
Space Banana <tomasbessa@protonmail.com>
2025-12-08 19:29:50 +0000

    Main menu easter egg

---
+++ EnemySelect.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+//Used in main menu, chooses whether to show Slenderman looking at the camera or someone else (easter egg)
+public class EnemySelect : MonoBehaviour
+{
+  public GameObject slender;
+  public GameObject easteregg;
+
+  //5% probability of easter egg taking Slenderman's place
+  void Awake() {
+    float result = Random.value;
+    if (result <= 0.95f) slender.active = true;
+    else easteregg.active = true;
+  }
+}
