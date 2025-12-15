2009e265e9a37e52a2126edd299acf87b2da69c4
Space Banana <tomasbessa@protonmail.com>
2025-12-15 16:14:09 +0000

    Further work on ghost code and generic enemy API

--- Enemy/EnemyAPI.cs
+++ Enemy/EnemyAPI.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
 
+//Library class for various enemy-related mechanics and behavior
+//Shared between different enemies in game to not duplicate code
 public class EnemyAPI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  public Transform player;
+  public Transform player_camera;
+  public Transform enemy;
+  public MeshRenderer enemy_mesh;
+  public CharacterController enemy_ctrl;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  public Player player_script;
+  public Pause pause_script;
+
+  private GameObject enemy_object;
+  
+  void Awake() {this.enemy_object = this.enemy.gameObject;}
+  
+  public float getDistance() {return Vector3.Distance(this.enemy.position, this.player.position);}
+
+  //Whether the enemy is inside the player's field of view, even if hidden behind an object
+  public bool isLookedAt() {return this.enemy_mesh.isVisible;}
+
+  //Whether the enemy is seen without being hidden behind an object
+  public bool isSeen() {
+    if (!isLookedAt()) {return false;}
+    Vector3[] ray_pos = {this.enemy.position, this.enemy.position, this.enemy.position};
+    ray_pos[1].y -= 0.4f;
+    ray_pos[2].y += 0.4f;
+    foreach (Vector3 pos in ray_pos) {
+      RaycastHit hit_info;
+      bool collided = Physics.Raycast(this.player_camera.position, pos-this.player_camera.position, out hit_info, 30);
+      if (!collided) {continue;}
+      if (hit_info.collider.gameObject == this.enemy_object) {return true;}
     }
+    return false;
+  }
+  
+  public void lookAtPlayer() {
+    Vector3 look_target = this.player.position;
+    look_target.y = this.enemy.position.y; //Do not rotate vertically
+    this.enemy.LookAt(look_target);
+  }
+
+  public void move(float speed, float gravity = -10) {
+    Vector3 motion = this.enemy.forward * speed;
+    motion.y = gravity;
+    this.enemy_ctrl.Move(motion * Time.deltaTime);  
+  }
+
+  public void teleport(float distance, float teleport_distance) {
+    this.enemy_ctrl.Move(this.enemy.forward * (distance-teleport_distance));
+    this.enemy_ctrl.Move(new Vector3(0, -100, 0)); //Gravity
+    lookAtPlayer();
+  }
+
+  //Disables player movement and positions the player to look at the enemy up close
+  public void killPlayer(float look_offset = 0.6f) {
+    this.pause_script.can_pause = false;
+    this.player_script.caught = true;
+    Vector3 enemy_pos = this.enemy.position;
+    enemy_pos.y = this.player_camera.position.y + look_offset; //Make the camera look slightly up
+    this.player_camera.LookAt(enemy_pos);
+    lookAtPlayer();
+  }
 }
--- Enemy/SCPGhost.cs
+++ Enemy/SCPGhost.cs
@@ -3,36 +3,64 @@ using UnityEngine;
 //Source code for the level 2 enemy behavior, a ghost inspired by SCP-087-1
 public class SCPGhost : MonoBehaviour
 {
-  public Transform player;
-  public Transform player_camera;
-  public CharacterController controller;
-  public MeshRenderer mesh;
+  public EnemyAPI enemy_api;
   public Material material;
 
-  float teleport_distance;
-  float speed = 1;
+  private float visible_percentage = 1; //For adjusting material transparency
+  private float teleport_distance;
+  private float speed = 1;
+
+  //Ghost turns invisible when it fades out, entering a cooldown where it's not hostile
+  private float invisible_cooldown = 20;
+  private float invisible_meter = 0;
+  private bool is_invisible = false;
   
   void Awake() {this.material.color = new Color32(109, 109, 109, 255);}
 
   void Update() {
-    lookAtPlayer();
-    if (this.mesh.isVisible) {return;}
-    Vector3 motion = this.transform.forward * this.speed;
-    motion.y = -10; //Gravity
-    this.controller.Move(motion * Time.deltaTime);        
+    this.enemy_api.lookAtPlayer();
+    float distance = this.enemy_api.getDistance();
+    bool is_visible = this.enemy_api.isLookedAt();
+    bool is_seen = this.enemy_api.isSeen();
+    
+    if (distance < 1.8f) {
+      this.enemy_api.killPlayer();
+      resetTransparency();
+      this.enabled = false;
+      return;
+    }
+    if (is_seen) {
+      if (distance < 4) fade(true);
+      else fade(false);
+      return;
+    }
+    fade(false);
+    this.enemy_api.move(this.speed);     
   }
 
-  //Duplicate code from Slenderman.cs
-  //Todo: make separate file containing look, kill, move and teleport logic, to share between different enemies
-  void lookAtPlayer() {
-    Vector3 look_target = this.player.position;
-    look_target.y = this.transform.position.y;
-    this.transform.LookAt(look_target);
+  void waitInvisibleTime() {
   }
 
-  void teleport(float distance) {
-    this.controller.Move(this.transform.forward * (distance-this.teleport_distance));
-    this.controller.Move(new Vector3(0, -100, 0)); //Gravity
-    lookAtPlayer();
+  //Ghost fades in or out, if fully transparent then it returns true
+  bool fade(bool fade_out) {
+    if (fade_out) {
+      if (this.visible_percentage < 0) this.visible_percentage = 0;
+      else this.visible_percentage -= 0.2f * Time.deltaTime;
+    }
+    else {
+      if (this.visible_percentage > 1) this.visible_percentage = 1;
+      else this.visible_percentage += 0.4f * Time.deltaTime;
+    }
+    Color c = this.material.color;
+    c.a = this.visible_percentage;
+    this.material.color = c;
+    return c.a == 0;
   }
+
+  void resetTransparency() {
+    Color c = this.material.color;
+    c.a = 1;
+    this.material.color = c;
+  }
+  
 }
