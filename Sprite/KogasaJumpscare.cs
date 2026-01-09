0252e1455db3a3e7ce64d4bf1a08c88455332d6a
Space Banana <tomasbessa@protonmail.com>
2026-01-09 18:22:55 +0000

    Level 3 easter egg

---
+++ Sprite/KogasaJumpscare.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+//Easter egg in level 3 ending
+public class KogasaJumpscare : MonoBehaviour
+{
+  public SpriteAPI sprite_drawing;
+  public SpriteAPI sprite_kogasa;
+  
+  public BlankScreen screen;
+  public AudioSource jumpscare_sound;
+
+  GameObject kogasa;
+  bool waitForPlayer = false;
+  bool jumpscare = false;
+  float look_time = 0;
+  
+  void Awake() {this.kogasa = this.sprite_kogasa.gameObject;}
+
+  void Update() {
+    if (this.jumpscare) {jumpscarePlayer();}
+    else if (this.waitForPlayer) { //Kogasa waits for player to look
+      if (!this.sprite_kogasa.isLookedAt()) return;
+      this.jumpscare_sound.Play();
+      this.jumpscare = true;
+      this.waitForPlayer = false;
+    }
+    else checkTrigger();
+  }
+
+  //Player needs to stare at the drawing from up close for 2 seconds
+  void checkTrigger() {
+    if (this.sprite_drawing.getDistance() > 4.5f) return;
+    if (this.sprite_drawing.isLookedAt()) this.look_time += 1 * Time.deltaTime;
+    if (this.look_time >= 1) {
+      this.kogasa.active = true;
+      this.waitForPlayer = true;
+    }
+  }
+
+  //Kogasa runs to player
+  void jumpscarePlayer() {
+    if (this.sprite_kogasa.getDistance() <= 1.7f) {
+      this.screen.displayWhiteScreen();
+      this.screen.fadeFromWhite(6);
+      this.jumpscare_sound.Stop();
+      this.kogasa.active = false;
+      this.enabled = false;
+      return;
+    }
+    this.sprite_kogasa.lookAtPlayer();
+    this.sprite_kogasa.move(14);
+  }
+}
--- Sprite/SpriteAPI.cs
+++ Sprite/SpriteAPI.cs
@@ -5,6 +5,10 @@ public class SpriteAPI : MonoBehaviour
 {
   public Transform player;
   public MeshRenderer mesh;
+  public bool movesInScene = false;
+  
+  private Transform sprite_t;
+  private CharacterController sprite_ctrl;
   
   private Color original_color;
   private Material material;
@@ -12,6 +16,10 @@ public class SpriteAPI : MonoBehaviour
   void Awake() {
     this.original_color = this.mesh.material.color;
     this.material = this.mesh.material;
+    
+    if (!this.movesInScene) return;
+    this.sprite_t = this.gameObject.GetComponent<Transform>();
+    this.sprite_ctrl = this.gameObject.GetComponent<CharacterController>();
   }
   
   public float getDistance() {return Vector3.Distance(this.transform.position, this.player.position);}
@@ -36,5 +44,27 @@ public class SpriteAPI : MonoBehaviour
     Color c = this.material.color;
     c.a = alpha;
     this.material.color = c;
+  }
+
+  //Motion-based functions, requires movesInScene set to true and requires a Transform and CharacterController components
+  public void move(float speed, float gravity = -10) {    
+    Vector3 motion = this.sprite_t.forward * speed;
+    motion.y = gravity;
+    this.sprite_ctrl.Move(motion * Time.deltaTime);
+  }
+
+  public void teleportToPlayer(float distance, bool forward, Terrain terrain = null, float height_correction = 1.5f) {
+    Vector3 direction = this.player.forward * distance;
+    if (!forward) direction *= -1;
+    
+    Vector3 new_position = this.player.position + direction;
+    if (terrain == null) new_position.y += height_correction;
+    else new_position.y = terrain.SampleHeight(new_position)+height_correction;
+
+    this.sprite_ctrl.enabled = false; //For manual position changes
+    this.sprite_t.position = new_position;
+    this.sprite_ctrl.enabled = true;
+    this.sprite_ctrl.Move(new Vector3(0, -100, 0)); //Gravity
+    lookAtPlayer();
   }
 }
